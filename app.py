35543b2f6568bdcb79991f42af92b350d5152606
Alexis Brouillette <alexisbrou1@hotmail.fr>
2024-09-05 21:04:56 -0400

    dd

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI, Request, Response
 import httpx
+from urllib.parse import urlparse, urlunparse
 
 app = FastAPI()
 
@@ -10,13 +11,15 @@ app = FastAPI()
 def hello_world():
     return "Hello,World"
 
-@app.post("/proxy")
-async def proxy_request(req: Request):
+@app.post("/proxy{path:path}")
+async def proxy_request(req: Request, path: str):
     target_url = "https://alexisbb-smartradio-api.hf.space"
+    target_path = urlparse(req.url).path
+    target_full_url = urlunparse((target_url, "", target_path, "", "", ""))
     async with httpx.AsyncClient() as client:
         proxy_res = await client.request(
             method=req.method,
-            url=target_url,
+            url=target_full_url,
             headers=req.headers,
             content=await req.body()
         )
